1b6008bc917dfb03113e0cfbb854b21d3b81ce77
FerShibli <fernandoluiz.1991@gmail.com>
2025-06-16 18:16:14 -0300

    feat(apiClient.js): implement apiFetch function for API requests

---
+++ public/apiClient.js
@@ -0,0 +1,19 @@
+export function apiFetch(path, options = {}) {
+  const API_BASE_URL =
+    window.location.hostname === "localhost"
+      ? "http://localhost:3000/api"
+      : `https://${window.location.hostname}/api`;
+  const url = `${API_BASE_URL}${path}`;
+  const fixedHeaders = {};
+  const token = localStorage.getItem("token");
+  if (token) {
+    fixedHeaders.Authorization = `Bearer ${token}`;
+  }
+  options.headers = {
+    ...options.headers,
+    ...fixedHeaders,
+    "Content-Type": "application/json",
+  };
+
+  return fetch(url, options);
+}
