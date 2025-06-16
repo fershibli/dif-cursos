19168350eb2ecba427922f9e9e63c121f14cbd60
FerShibli <fernandoluiz.1991@gmail.com>
2025-06-16 16:14:51 -0300

    fix(server.ts): correct import order and formatting for consistency

--- api/server.ts
+++ api/server.ts
@@ -1,14 +1,16 @@
-import express from "express"
-import cors from "cors"
-import path from "path"
-import cursoRoutes from "./routes/cursos.routes"
+import express from "express";
+import cors from "cors";
+import path from "path";
+import cursoRoutes from "./routes/cursos.routes";
+import usuarioRoutes from "./routes/usuarios.routes";
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static(path.join(__dirname, "../public")))
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.use("/api/cursos", cursoRoutes)
+app.use("/api/usuarios", usuarioRoutes);
+app.use("/api/cursos", cursoRoutes);
 
-export default app
+export default app;
